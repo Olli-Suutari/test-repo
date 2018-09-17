fad5943891dbfd346edf17e9c24f21705804110d
Olli Suutari <olli.suutari@jkl.fi>
2018-09-17 16:55:47 +0300

    SPA version
    SPA version

--- testCode/js/index.js
+++ testCode/js/index.js
@@ -12,7 +12,7 @@ function getSyncScriptParams() {
 }
 getSyncScriptParams();
 
-function toggleFullScreen() {
+function toggleFullScreen(target) {
     // if already full screen; exit
     // else go fullscreen
     if (
@@ -31,7 +31,7 @@ function toggleFullScreen() {
         document.msExitFullscreen();
       }
     } else {
-      element = $('#sliderBox').get(0);
+      element = $( target ).get(0);
       if (element.requestFullscreen) {
         element.requestFullscreen();
       } else if (element.mozRequestFullScreen) {
@@ -44,8 +44,39 @@ function toggleFullScreen() {
     }
 }
 
-// jsonp_url base
-var jsonp_url = "https://api.kirjastot.fi/v3/library/" + library + "?lang=" + lang
+// Function for adding a new palvelut item.
+function addItem(item, listElement) {
+    var name = item.name;
+    // Use "custom_name", where available.
+    if (item.custom_name != null) {
+        name = item.custom_name;
+    }
+    // Add popup link if additional details are available.
+    if(item.short_description != null) {
+        var description = item.short_description;
+        var websiteLink = item.website;
+        // Add "long" description where available.
+        if (item.description != null) {
+            // Replace row splits with <br>
+            var longDescription = item.description.replace(/\r\n/g, "<br>");
+            description = description + '<br><br>' + longDescription;
+        }
+        // Add price where available.
+        if (item.price != null) {
+            description = description + '<br><br>Hintatiedot: ' +  item.price;
+        }
+        // Add the item to a desired element.
+        $( listElement ).append('<li> ' + 
+        '<a class="index-item" data-message="' + description + '" data-website="' + websiteLink + '" tabindex="0" href="#"' +
+        'role="button" aria-expanded="false" aria-controls="' + name + '"' +
+        'title="' + name + '">' + name + '</a></li>');
+    // If no description found, don't create the link
+    } else {
+        $( listElement ).append('<li> ' + 
+        name + '</li>');
+    }
+}
+
 var weekCounter = 0;
 function getWeekSchelude(direction) {
     // +1 or -1;
@@ -224,7 +255,12 @@ function getWeekSchelude(direction) {
     });
 }
 
+// jsonp_url base
+var jsonp_url = "https://api.kirjastot.fi/v3/library/" + library + "?lang=" + lang
 $(document).ready(function($) {
+         /*
+         Esittely 
+         */
         // Generic details
         $.getJSON(jsonp_url + "&with=extra", function(data) { 
             $( ".library-slogan" ).append( data.extra.slogan );
@@ -324,5 +360,120 @@ $(document).ready(function($) {
         });
         // Scheludes
         getWeekSchelude(0);
-    });
+        /*
+         Yhteystiedot 
+         */
+        // Generic details
+        $.getJSON(jsonp_url + "&with=mail_address", function(data) {
+            $( "#streetAddress" ).append( data.name  + '<br>' + data.address.street + '<br>' + data.address.zipcode + ' ' + data.address.city);
+            $( "#postalAddress" ).append( data.name  + '<br>PL ' + data.mail_address.box_number + '<br>' + data.mail_address.zipcode + ' ' + data.mail_address.area);
+            $( "#email" ).append( data.email );
 
+            // Map coordinates (marker)
+            var lon = data.address.coordinates.lon;
+            var lat = data.address.coordinates.lat;
+            // Position, 5 decimal degrees
+            var lonDecimal = parseFloat(lon.match(/[\d][\d][^\d][\d][\d][\d][\d][\d]/));
+            var latDecimal = parseFloat(lat.match(/[\d][\d][^\d][\d][\d][\d][\d][\d]/));
+
+            // Generate the box around the marker by +- 0.0018 lat/long
+            var lonBoxStart = lonDecimal - 0.0018;
+            var lonBoxEnd = lonDecimal + 0.0018;
+            var latBoxStart = latDecimal - 0.0018;
+            var latBoxEnd = latDecimal + 0.0018;
+            // Append the map to the container
+            $("#map-container").append('<iframe id="map-frame" width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=' + lonBoxStart + '%2C' + latBoxStart + '%2C' + lonBoxEnd + '%2C' + latBoxEnd + '&amp;layer=mapnik&amp;marker=' + lat + '%2C' + lon + '" style="border: 1px solid black"></iframe>')
+
+        });
+        // Phone numbers.
+        $.getJSON(jsonp_url + "&with=phone_numbers", function(data) {
+            for (var i=0; i<data.phone_numbers.length; i++) {
+                $( "#phoneNumbers" ).append( '<tr>' + 
+                '<td>' + data.phone_numbers[i].name + '</td>' +
+                '<td>' + data.phone_numbers[i].number + '</td>' +
+                '</tr>' );
+            }
+        });
+        // Staff list
+        $.getJSON(jsonp_url + "&with=persons", function(data) {
+            for (var i=0; i<data.persons.length; i++) {
+                $( "#staffMembers" ).append( '<tr>' + 
+                '<td>' + data.persons[i].first_name + ' ' + data.persons[i].first_name + '</td>' +
+                '<td>' + data.persons[i].job_title + '</td>' +
+                '<td>' + data.persons[i].email + '</td>' +
+                '</tr>' );
+            }
+        });
+
+        /*
+         Palvelut 
+        */
+        $.getJSON(jsonp_url + "&with=services", function(data) {
+            var collectionCount = 0;
+            var hardwareCount = 0;
+            var roomCount = 0;
+            var serviceCount = 0;
+            for (var i=0; i<data.services.length; i++) {
+                // Collections
+                if (data.services[i].type == "collection") {
+                    collectionCount = collectionCount +1;
+                    addItem(data.services[i], '#collectionItems');
+                }
+                // Hardware
+                else if (data.services[i].type == "hardware") {
+                    hardwareCount = hardwareCount +1;
+                    addItem(data.services[i], '#hardwareItems');
+                }
+                // Rooms
+                else if (data.services[i].type == "room") {
+                    roomCount = roomCount +1;
+                    addItem(data.services[i], '#roomItems'); 
+                }
+                // Services
+                else if (data.services[i].type == "service") {
+                    serviceCount = serviceCount +1;
+                    addItem(data.services[i], '#serviceItems');  
+                }
+            }
+            // Show titles & counts if found.
+            if (collectionCount != 0) {
+                $( "#collection" ).css('display', 'block');
+                $( "#collectionBadge" ).append('(' + collectionCount + ')');
+            }
+            if (hardwareCount != 0) {
+                $( "#hardware" ).css('display', 'block');
+                $( "#hardwareBadge" ).append('(' + hardwareCount + ')');
+            }
+            if (roomCount != 0) {
+                $( "#room" ).css('display', 'block');
+                $( "#roomBadge" ).append('(' + roomCount + ')');
+            }
+            if (serviceCount != 0) {
+                $( "#service" ).css('display', 'block');
+                $( "#serviceBadge" ).append('(' + serviceCount + ')');
+            }
+            
+            // When item link is clicked.
+            $( ".index-item" ).on('click', function () {
+                // If infobox already visible, hide it instantly to avoid wonky animations.
+                if(isInfoBoxVisible) {
+                    toggleInfoBox(0);
+                }
+                // Get element position
+                var posX = $(this).offset().left,
+                // Set top to be slightly under the element
+                posY = $(this).offset().top + 20;
+                // Set popup position & content, toggle visibility.
+                $( "#infoPopup" ).css ('transform', 'translate3d(' + posX + 'px,' + posY + 'px, 0px')
+                $( "#popover-content" ).html( '<p id="popover-content">' + $(this).data('message') + '</p>');
+                // If website is not null and contains stuff. Sometimes empty website is shown unless lenght is checked.
+                if ($(this).data('website') != null && $(this).data('website').length > 5) {
+                    $( "#link-to-info" ).html( '<p id="link-to-info"><a href="' + $(this).data('website') + 
+                    '" class="external-link">Lisätietoja</a></p>');
+                } else {
+                    $( "#link-to-info" ).html( '<p id="link-to-info"></p>');
+                }
+                toggleInfoBox(100);
+            });
+        });
+    });
