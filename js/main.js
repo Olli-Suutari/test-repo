ad31b05146cf3030865e4ad4c5e32aed6e5670b8
JK\suutarol <olli.suutari@jkl.fi>
2018-10-24 13:47:31 +0300

    Same

--- js/main.js
+++ js/main.js
@@ -608,10 +608,10 @@ $(document).ready(function() {
             function detectswipe(el,func) {
                 swipe_det = new Object();
                 swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
-                var min_x = 30;  //min x swipe for horizontal swipe
-                var max_x = 30;  //max x difference for vertical swipe
-                var min_y = 80;  //min y swipe for vertical swipe
-                var max_y = 90;  //max y difference for horizontal swipe
+                var min_x = 40;  //min x swipe for horizontal swipe
+                var max_x = 40;  //max x difference for vertical swipe
+                var min_y = 150;  //min y swipe for vertical swipe
+                var max_y = 1600;  //max y difference for horizontal swipe
                 var direc = "";
                 ele = document.getElementById(el);
                 ele.addEventListener('touchstart',function(e){
