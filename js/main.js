b53911126dc6c31cae1c63173605a743341f8237
JK\suutarol <olli.suutari@jkl.fi>
2018-10-24 14:26:14 +0300

    Tweaks

--- js/main.js
+++ js/main.js
@@ -609,9 +609,9 @@ $(document).ready(function() {
                 swipe_det = new Object();
                 swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
                 var min_x = 40;  //min x swipe for horizontal swipe
-                var max_x = 40;  //max x difference for vertical swipe
-                var min_y = 60;  //min y swipe for vertical swipe (ignored)
-                var max_y = 70;  //max y difference for horizontal swipe (ignored)
+                var max_x = 1;  //max x difference for vertical swipe
+                var min_y = 1;  //min y swipe for vertical swipe (ignored)
+                var max_y = 50;  //max y difference for horizontal swipe (ignored)
                 var direc = "";
                 ele = document.getElementById(el);
                 ele.addEventListener('touchstart',function(e){
@@ -649,7 +649,7 @@ $(document).ready(function() {
 
             function myfunction(el,d) {
                 if (el === "schedules") {
-                    alert("Thou have swiped on element with id '"+el+"' to "+d+" direction");
+                    alert("Thou swiped on element with id '"+el+"' to "+d+" direction");
                     if(d === "r") {
                         $("#lastWeek").focus();
                         $("#lastWeek").click();
