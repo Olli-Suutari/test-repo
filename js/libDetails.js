aa15db7d3a9a2b9a9ef4a364d1bf635be0d63d07
Olli Suutari <olli.suutari@jkl.fi>
2019-02-19 14:18:54 +0200

    Update libDetails.js

--- js/libDetails.js
+++ js/libDetails.js
@@ -492,6 +492,7 @@ function generateImages(data) {
             $(".rslides").append('<li><img src="' + data.pictures[i].files.medium + '" alt="' + altText + '"></li>');
             counter = counter +1;
             if(counter === data.pictures.length) {
+                console.log("DEFER:");
                 imageListDeferred.resolve();
             }
         }
@@ -504,7 +505,8 @@ function asyncFetchImages() {
     var imagesDeferred = jQuery.Deferred();
     setTimeout(function() {
             $.getJSON(jsonp_url + "&with=pictures", function (data) {
-                console.log("F: " + jsonp_url);
+                console.log("F: " + jsonp_url + "&with=pictures");
+                console.log(data);
                 // If no pictures found, hide the slider...
                 if (data.pictures.length === 0) {
                     $('#sliderBox').css('display', 'none');
