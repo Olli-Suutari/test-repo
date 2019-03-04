311b1541abe80f20f71533b9702a3ebbd9ec4b56
Olli Suutari <olli.suutari@jkl.fi>
2019-03-04 10:48:54 +0200

    c

--- js/homePage.js
+++ js/homePage.js
@@ -72,7 +72,7 @@ $(document).ready(function() {
 
     setTimeout(function(){
         var bodyHeight = $( "body" ).height() -15;
-        if(bodyHeight > 300) {
+        if(bodyHeight > 235) {
             $('#homePageWidget').css("min-height", bodyHeight);
             console.log("bodyHeight " + bodyHeight);
         }
